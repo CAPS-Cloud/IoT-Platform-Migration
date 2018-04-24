006ecfa036c81e5e679f41c77bd2f94b8cf67699
heldic <helge@dickel.io>
2018-04-24 21:42:47 +0200

    replace add with wget - enable chaching

--- iotplatform/flink/start.sh
+++ iotplatform/flink/start.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-cd /docker/flink/flink-1.3.2
+cd /flink/flink-1.4.2
 
 ./bin/start-local.sh
-
-./bin/flink run -c de.tu.dresden.TestStreamConsumer /docker/proj/target/kpstreambench-flink-1.0-SNAPSHOT.jar
 
 # don't exit
 /usr/bin/tail -f /dev/null
