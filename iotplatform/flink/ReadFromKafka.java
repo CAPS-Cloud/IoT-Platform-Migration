901a8c38ce3326049b00cf4c3ecdb627be141386
heldic <helge@dickel.io>
2018-04-28 14:44:27 +0200

    minor

--- iotplatform/flink/ReadFromKafka.java
+++ iotplatform/flink/ReadFromKafka.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
@@ -52,18 +35,19 @@ import java.util.Map;
  */
 public class ReadFromKafka {
     public static void main(String[] args) throws Exception {
-        // create execution environment
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        // parse user parameters
         ParameterTool parameterTool = ParameterTool.fromArgs(args);
-        // Elastic search
+
         Map<String, String> config = new HashMap<>();
         config.put("cluster.name", "elasticsearch");
-        // This instructs the sink to emit after every element, otherwise they would be buffered
         config.put("bulk.flush.max.actions", "1");
+
         List<InetSocketAddress> transportAddresses = new ArrayList<>();
+        // transportAddresses.add(new InetSocketAddress(InetAddress.getByName("elasticsearch"), 9300));
         transportAddresses.add(new InetSocketAddress(InetAddress.getByName("elasticsearch"), 9300));
+        
         System.out.println("Elasticsearch detected at: " + InetAddress.getByName("elasticsearch").toString());
+
         DataStream<String> messageStream = env.addSource(
                 new FlinkKafkaConsumer011<>(
                         parameterTool.getRequired("topic"),
