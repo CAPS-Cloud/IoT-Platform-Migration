a7b7a1a346d875503b34656d5e322a31046970cd
Hans Kirchner <hans-kirchner@gmx.de>
2018-05-05 12:02:13 +0200

    Add python producer -> websocket

---
+++ iotplatform/python_producer/main.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+import time
+import random
+
+def main():
+    async def hello(uri):
+        async with websockets.connect(uri) as websocket:
+            while True:
+                await websocket.send(json.dumps(generate_msg()))
+                await asyncio.sleep(1)
+
+    asyncio.get_event_loop().run_until_complete(
+        hello('ws://iotbridge:8765'))
+
+def generate_msg():
+    msg = {
+        "sensorGroup" : "pythonGroup",
+        "sensorId" : "fakeWeather",
+        "timestamp" : time.time(),
+        "reading" : str(random.randint(0,100))
+    }
+    return msg
+
+if __name__ == "__main__":
+    main()
--- iotplatform/python_producer/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python --version
-
-# don't exit
-/usr/bin/tail -f /dev/null
