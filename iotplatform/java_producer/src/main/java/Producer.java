42267dc39cc4ad9b15b817e62d6a3c724817311c
Hans Kirchner <hans-kirchner@gmx.de>
2018-05-05 13:40:05 +0200

    Add Java Producer

---
+++ iotplatform/java_producer/src/main/java/Producer.java
@@ -0,0 +1,45 @@
+import com.google.gson.Gson;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+
+public class Producer {
+    public static void main(String args[]) {
+        Gson gson = new Gson();
+
+        while(true) {
+            try {
+                String message = gson.toJson(generateEvent());
+
+                Content response = Request.Post(args[0])
+                        .bodyString(message,ContentType.APPLICATION_JSON)
+                        .execute().returnContent();
+                System.out.println(response.asString());
+            } catch(Exception e) {
+                System.err.println(e);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static class SensorEvent {
+        public String sensorGroup;
+        public String sensorId;
+        public long timestamp;
+        public String reading;
+    }
+
+    private static SensorEvent generateEvent() {
+        SensorEvent evt = new SensorEvent();
+        evt.sensorGroup = "javaGroup";
+        evt.sensorId = "fakeWeather";
+        evt.timestamp = System.currentTimeMillis();
+        evt.reading = Double.toString(Math.random() * 100);
+        return evt;
+    }
+}
--- iotplatform/java_producer/start.sh
+++ iotplatform/java_producer/start.sh
@@ -2,5 +2,7 @@
 
 java -version
 
+java -jar target/java_producer-1.0.jar http://iotbridge:8083
+
 # don't exit
 /usr/bin/tail -f /dev/null
