27f6ff5f8df8840b9d2111e038ec4ed670825ed5
heldic <helge@dickel.io>
2018-05-16 00:11:46 +0200

    update .travis.yml

--- travis/push-docker-images-to-dockerhub.sh
+++ travis/push-docker-images-to-dockerhub.sh
@@ -2,3 +2,7 @@
 set -e
 
 docker login --username $DOCKERHUB_USER --password $DOCKERHUB_PW
+
+docker build -t ${DOCKERHUB_USER}/${DOCKERHUB_REPOSITORY}:$TRAVIS_COMMIT -f ${BUILD_PATH} .
+
+docker push ${DOCKERHUB_USER}/${DOCKERHUB_REPOSITORY}
