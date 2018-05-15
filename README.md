d8c7e1eb71cb58f704ffb2e9613594e8371091f1
heldic <helge@dickel.io>
2018-05-16 00:18:45 +0200

    update .travis.yml

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # iotplatform
+[![Build Status](https://travis-ci.com/heldic/iotplatform.svg?token=UgRpWYHRU3yqYszd3B6x&branch=master)](https://travis-ci.com/heldic/iotplatform)
 
 ## Architecture
 <img src="./doc/iotplatform.png" />
--- travis/push-docker-images-to-dockerhub.sh
+++ travis/push-docker-images-to-dockerhub.sh
@@ -3,6 +3,6 @@ set -e
 
 docker login --username $DOCKERHUB_USER --password $DOCKERHUB_PW
 
-docker build -t ${DOCKERHUB_PROJECT}/${DOCKERHUB_REPOSITORY}:$TRAVIS_COMMIT -f ${BUILD_PATH} .
+docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT -f ${BUILD_PATH} .
 
-docker push ${DOCKERHUB_PROJECT}/${DOCKERHUB_REPOSITORY}
+docker push ${DOCKERHUB_USER}/${IMAGE_NAME}
