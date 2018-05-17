a77b094850ffdc138daab8005aaab9002fbe03d4
heldic <helge@dickel.io>
2018-05-17 21:54:17 +0200

    test docker cache in travis

--- travis/push-docker-images-to-dockerhub.sh
+++ travis/push-docker-images-to-dockerhub.sh
@@ -5,8 +5,10 @@ docker login --username ${DOCKERHUB_USER} --password $DOCKERHUB_PW
 
 cd ${BUILD_PATH}
 
-docker pull ${DOCKERHUB_USER}/${IMAGE_NAME}
+#docker pull ${DOCKERHUB_USER}/${IMAGE_NAME}:latest
 
-docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT ./ --cache-from ${DOCKERHUB_USER}/${IMAGE_NAME}
+#docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT -t ${DOCKERHUB_USER}/${IMAGE_NAME}:latest ./ --cache-from ${DOCKERHUB_USER}/${IMAGE_NAME}
+
+docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT -t ${DOCKERHUB_USER}/${IMAGE_NAME}:latest ./ 
 
 docker push ${DOCKERHUB_USER}/${IMAGE_NAME}
