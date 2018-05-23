f87e15da815e94358bb341bc48bafcb26877c97d
heldic <helge@dickel.io>
2018-05-23 23:30:51 +0200

    include docker pull - save time

--- travis/push-docker-images-to-dockerhub.sh
+++ travis/push-docker-images-to-dockerhub.sh
@@ -5,10 +5,10 @@ docker login --username ${DOCKERHUB_USER} --password $DOCKERHUB_PW
 
 cd ${BUILD_PATH}
 
-#docker pull ${DOCKERHUB_USER}/${IMAGE_NAME}:latest
+docker pull ${DOCKERHUB_USER}/${IMAGE_NAME}:latest
 
-#docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT -t ${DOCKERHUB_USER}/${IMAGE_NAME}:latest ./ --cache-from ${DOCKERHUB_USER}/${IMAGE_NAME}:latest
+docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT -t ${DOCKERHUB_USER}/${IMAGE_NAME}:latest ./ --cache-from ${DOCKERHUB_USER}/${IMAGE_NAME}:latest
 
-docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT -t ${DOCKERHUB_USER}/${IMAGE_NAME}:latest ./
+#docker build -t ${DOCKERHUB_USER}/${IMAGE_NAME}:$TRAVIS_COMMIT -t ${DOCKERHUB_USER}/${IMAGE_NAME}:latest ./
 
 docker push ${DOCKERHUB_USER}/${IMAGE_NAME}
