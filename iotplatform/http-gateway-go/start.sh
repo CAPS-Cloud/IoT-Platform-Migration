0ed7fefe7c2195edfbc18648a70c3a42085af865
cgebe <gebendorfer@icewave.org>
2018-06-10 22:08:56 +0200

    http go gateway without auth

--- iotplatform/http-gateway-go/start.sh
+++ iotplatform/http-gateway-go/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-/wait-for-it.sh $ZOOKEEPER --timeout=0 --strict
-/wait-for-it.sh $KAFKA --timeout=0 --strict
+./wait-for-it.sh $ZOOKEEPER --timeout=0 --strict
+./wait-for-it.sh $KAFKA --timeout=0 --strict
 
-cd /app
+cd ./app
 
-go run main.go
+go run main.go --zookeeper $ZOOKEEPER --port 8084
 
 # don't exit
 /usr/bin/tail -f /dev/null
