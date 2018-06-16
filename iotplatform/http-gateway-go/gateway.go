28e3f448949463fd46cfd7964a1cb3d3da74d49f
cgebe <gebendorfer@icewave.org>
2018-06-16 15:57:24 +0200

    jwt token auth - http-gateway-go

--- iotplatform/http-gateway-go/gateway.go
+++ iotplatform/http-gateway-go/gateway.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"crypto/rsa"
 	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/Shopify/sarama"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 type Gateway struct {
-	Producer sarama.AsyncProducer
+	PublicKey *rsa.PublicKey
+	Producer  sarama.AsyncProducer
 }
 
 func (g *Gateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +24,20 @@ func (g *Gateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		token := strings.Replace(string(ctx.Request.Header.Peek("Authorization")), "Bearer ", "", -1)
 		fmt.Println(token)
 	*/
+
+	// validate the token
+	token, err := jwt.Parse("ey", func(token *jwt.Token) (interface{}, error) {
+		// since we only use the one private key to sign the tokens,
+		// we also only use its public counter part to verify
+		return g.PublicKey, nil
+	})
+
+	if token.Valid {
+		log.Println("valid token")
+	} else {
+		log.Println("invalid token")
+	}
+
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
---
+++ iotplatform/http-gateway-go/jwt.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crypto/rsa"
+	"io/ioutil"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+const (
+	pubKeyPath = "keys/app.rsa.pub" // openssl rsa -in app.rsa -pubout > app.rsa.pub
+)
+
+func LoadPublicKey() *rsa.PublicKey {
+	bytes, err := ioutil.ReadFile(pubKeyPath)
+	if err != nil {
+		panic(err)
+	}
+
+	key, err := jwt.ParseRSAPublicKeyFromPEM(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return key
+}
