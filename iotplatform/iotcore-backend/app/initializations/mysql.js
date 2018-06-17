ad8e9fd0059b89dbd9b7767306793709c154d1b2
bhhoseini <bh.hoseini@gmail.com>
2018-06-17 17:51:03 +0200

    adding sensor to model

--- iotplatform/iotcore-backend/app/initializations/mysql.js
+++ iotplatform/iotcore-backend/app/initializations/mysql.js
@@ -1,11 +1,13 @@
 
 const Users = require('../models/UsersModel');
 const Devices = require('../models/DevicesModel');
+const Sensors = require('../models/SensorsModel')
 
 
 async function syncDataModels() {
     await Users.sync({ force: true });
     await Devices.sync({ force: true });
+    await Sensors.sync({force:true});
 }
 
 syncDataModels();
