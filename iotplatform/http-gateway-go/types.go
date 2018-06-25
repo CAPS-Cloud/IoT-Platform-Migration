1e8906fd0cda50208ab6a594ea8f9e1a31690b02
cgebe <gebendorfer@icewave.org>
2018-06-25 11:59:26 +0200

    jwt authentication optional for benchmark, serialize/deserialize message for path construction, path construction, message struct

--- iotplatform/http-gateway-go/types.go
+++ iotplatform/http-gateway-go/types.go
@@ -1,4 +1,9 @@
 package main
 
 type Message struct {
+	Topic       string `json: "topic"`
+	SensorID    string `json:"sensorId"`
+	SensorGroup string `json:"sensorGroup"`
+	Timestamp   int64  `json:"timestamp"`
+	Value       string `json:"reading"`
 }
