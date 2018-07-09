d9a6c4a32f3afcaa77e36dcd2a4400642446942e
heldic <helge@dickel.io>
2018-07-09 12:49:02 +0200

    add auth to http-gateway-nodejs

--- iotplatform/http-gateway-nodejs/app/index.js
+++ iotplatform/http-gateway-nodejs/app/index.js
@@ -1,11 +1,13 @@
-var express = require('express')
-var kafka = require("kafka-node")
-var bodyParser = require("body-parser")
-var grpc = require('grpc');
+const express = require('express');
+const kafka = require("kafka-node");
+const bodyParser = require("body-parser");
+const grpc = require('grpc');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
-var httpPort = 8083
-var PROTO_PATH = __dirname + '/protos/helloworld.proto';
-var hello_proto = grpc.load(PROTO_PATH).helloworld;
+const httpPort = 8083
+
+const key = fs.readFileSync('./.keys/jwtRS256.key.pub');  // get public key
 
 var httpServer, kafkaProducer, kafkaClient
 
@@ -13,82 +15,82 @@ const args = process.argv;
 const ZOOKEEPER = args[2];
 const IOTCORE_BACKEND = args[3];
 
-async function initGRPC() {
-  return new Promise((resolve) => {
-    let client = new hello_proto.Greeter(IOTCORE_BACKEND,
-                                         grpc.credentials.createInsecure())
-    let user
-    if (process.argv.length >= 3) {
-      user = process.argv[2]
-    } else {
-      user = 'world'
-    }
-    client.sayHello({name: user}, function(err, response) {
-      //console.log('Greeting:', response.message)
-      console.log("client.sayHello")
-    })
-    resolve()
-  })
-}
-
 async function initRest() {
-  console.log("attempting to initiate http server...")
-  return new Promise((resolve) => {
-    const app = express()
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
+    console.log("attempting to initiate http server...")
+    return new Promise((resolve) => {
+        const app = express()
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({ extended: true }))
 
-    app.post("/", function (req, res) {
-      forwardMsg(JSON.stringify(req.body))
-      res.status(200).send('OK')
-      res.end()
-    })
+        app.post("/", function (req, res) {
+            console.log(req.headers.authorization)
+            if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+                // verify a token asymmetric
+                jwt.verify(req.headers.authorization.split(' ')[1], key, function(err, decoded) {
+                    if(!(err != null)) {
+                        if(decoded.iss === "iotplatform") {
+                            forwardMsg(JSON.stringify(req.body), decoded.sub)
+                            res.status(200).send('OK');
+                            res.end();
+                        }
+                    } else {
+                        console.log("403 - Forbidden");
+                        res.status(403).send('Forbidden');
+                        res.end();
+                    }
+                });
+            } else {
+                console.log("401 - Unauthorized");
+                res.status(401).send('Unauthorized');
+                res.end();
+            }
+        })
 
-    httpServer = app.listen(httpPort, () => {
-      console.log("app running on port ", httpServer.address().port)
+        httpServer = app.listen(httpPort, () => {
+            console.log("app running on port ", httpServer.address().port)
+        })
+        resolve()
     })
-    resolve()
-  })
 }
 
 async function initKafka() {
-  return new Promise((resolve) => {
-    console.log("attempting to initiate Kafka connection...")
-    kafkaClient = new kafka.Client(ZOOKEEPER)
+    return new Promise((resolve) => {
+        console.log("attempting to initiate Kafka connection...")
+        kafkaClient = new kafka.Client(ZOOKEEPER)
 
-    kafkaProducer = new kafka.HighLevelProducer(kafkaClient)
-    kafkaProducer.on("ready", () => {
-        console.log("kafka producer is connected and ready")
+        kafkaProducer = new kafka.HighLevelProducer(kafkaClient)
+        kafkaProducer.on("ready", () => {
+            console.log("kafka producer is connected and ready")
+        })
+        kafkaProducer.on('ready', () => {
+          resolve()
+        })
     })
-    kafkaProducer.on('ready', () => {
-      resolve()
-    })
-  })
 }
 
-function forwardMsg(message) {
-  let payloads
-  if(typeof(message) === "object") {
-    payloads = [
-      { topic: 'livedata', messages: message.toString() }
-    ]
-  } else if(typeof(message) === "string") {
-    payloads = [
-      { topic: 'livedata', messages: message }
-    ]
-  } else {
-    console.log("invalid type of data - not forwarded to kafka")
-    return
-  }
+function forwardMsg(message, deviceId) {
+    let payloads, messageString
+    if(typeof(message) === "object") {
+        messageString = message.toString()
+    } else if(typeof(message) === "string") {
+        messageString = message
+    } else {
+        console.log("invalid type of data - not forwarded to kafka")
+        return
+    }
 
-  kafkaProducer.send(payloads, function (err, data) {
-    console.log("forwarded to kafka:")
-    console.log(payloads)
-  })
+    payloads = [
+        { topic: deviceId + "_" + JSON.parse(messageString).sensor_id, messages: messageString }
+    ]
+
+    kafkaProducer.send(payloads, function (err, data) {
+        console.log("forwarded to kafka:")
+        console.log(payloads)
+    })
 }
 
-Promise.all([initKafka(), initGRPC()]).then(() => {
-  initRest().then(() => {
+Promise.all([initKafka()]).then(() => {
+    initRest().then(() => {
 
-  })
+    })
 })
