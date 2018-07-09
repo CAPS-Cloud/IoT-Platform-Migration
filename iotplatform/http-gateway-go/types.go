610328b121ba3a66d6644d10692e2cd142be0c37
cgebe <gebendorfer@icewave.org>
2018-07-09 11:18:20 +0200

    ws gateway go, type changes, package structure change

--- iotplatform/http-gateway-go/types.go
+++ iotplatform/http-gateway-go/types.go
@@ -1,8 +1,8 @@
 package main
 
 type Message struct {
-	DeviceID  string `json: "device_id"`
-	SensorID  string `json:"sensor_id"`
-	Timestamp int64  `json:"timestamp"`
-	Value     string `json:"value"`
+	DeviceID  string  `json: "device_id"`
+	SensorID  int64   `json:"sensor_id"`
+	Timestamp int64   `json:"timestamp"`
+	Value     float64 `json:"value"`
 }
