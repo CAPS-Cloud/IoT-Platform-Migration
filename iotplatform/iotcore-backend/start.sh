521b4f977e5e001f18bd77b432e235f32611ba8c
Peeranut Chindanonda <pcxd@me.com>
2018-07-14 22:39:41 +0200

    Backend wait for all service

--- iotplatform/iotcore-backend/start.sh
+++ iotplatform/iotcore-backend/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 /wait-for-it.sh $MARIADB --timeout=0 --strict
+/wait-for-it.sh $KAFKA --timeout=0 --strict
+/wait-for-it.sh $ZOOKEEPER --timeout=0 --strict
+/wait-for-it.sh $ELASTICSEARCH --timeout=0 --strict
+/wait-for-it.sh $FLINK --timeout=0 --strict
 
 cd app
 
