f131fdca38620686fd84a982745b7a9830506fb3
heldic <helge@dickel.io>
2018-07-15 20:41:44 +0200

    ws gateway secure & array support

--- iotplatform/ws-gateway-nodejs/app/index.js
+++ iotplatform/ws-gateway-nodejs/app/index.js
@@ -1,101 +1,98 @@
-var kafka = require("kafka-node");
-var WebSocket = require('ws');
+const kafka = require("kafka-node");
+const WebSocket = require('ws');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-
-var wsPort = 8765
 
 var wsserver, kafkaProducer, kafkaClient
 
 const key = fs.readFileSync('./.keys/jwtRS256.key.pub');  // get public key
+const wsPort = 8765;
 
 const args = process.argv;
 const ZOOKEEPER = args[2];
 const IOTCORE_BACKEND = args[3];
 
 async function initWebSocket() {
-  console.log("attempting to initiate ws server...")
-  return new Promise((resolve) => {
-    wsserver = new WebSocket.Server({ port: wsPort })
-    resolve()
-  })
+    console.log("attempting to initiate ws server...");
+    return new Promise((resolve) => {
+        wsserver = new WebSocket.Server({
+            port: wsPort,
+            verifyClient: (info, cb) => {
+                if(info.req.headers.authorization && info.req.headers.authorization.split(' ')[0] === 'Bearer') {
+                    jwt.verify(info.req.headers.authorization.split(' ')[1], key, function(err, decoded) {
+                        if(!(err != null)) {
+                            info.req.user = decoded;
+                            console.log("200 - OK");
+                            cb(true);
+                        } else {
+                            console.log("403 - Forbidden");
+                            cb(false, 403, 'Forbidden');
+                        }
+                    });
+                } else {
+                    console.log("401 - Unauthorized");
+                    cb(false, 401, 'Unauthorized');
+                }
+            }
+        });
+        resolve();
+    })
 }
 
 async function initKafka() {
-  return new Promise((resolve) => {
-    console.log("attempting to initiate Kafka connection...")
-    kafkaClient = new kafka.Client(ZOOKEEPER)
+    return new Promise((resolve) => {
+        console.log("attempting to initiate Kafka connection...");
+        kafkaClient = new kafka.Client(ZOOKEEPER);
 
-    kafkaProducer = new kafka.HighLevelProducer(kafkaClient)
-    kafkaProducer.on("ready", () => {
-        console.log("kafka producer is connected and ready")
+        kafkaProducer = new kafka.HighLevelProducer(kafkaClient);
+        kafkaProducer.on("ready", () => {
+            console.log("kafka producer is connected and ready");
+        })
+        kafkaProducer.on('ready', () => {
+            resolve();
+        })
     })
-    kafkaProducer.on('ready', () => {
-      resolve()
-    })
-  })
 }
 
 function forwardMsg(message, deviceId) {
-    let payloads, messageString
+    let payloads, messageString;
 
     if(typeof(message) === "object") {
-        messageString = message.toString()
+        messageString = message.toString();
     } else if(typeof(message) === "string") {
-        messageString = message
+        messageString = message;
     } else {
-        console.log("invalid type of data - not forwarded to kafka")
-        return
+        console.log("invalid type of data - not forwarded to kafka");
+        return;
     }
 
     if(Array.isArray(JSON.parse(messageString))) {
         payloads = [
             { topic: deviceId + "_" + JSON.parse(messageString)[0].sensor_id, messages: messageString }
-        ]
+        ];
     } else {
         payloads = [
             { topic: deviceId + "_" + JSON.parse(messageString).sensor_id, messages: messageString }
-        ]
+        ];
     }
 
     kafkaProducer.send(payloads, (err) => {
         if(err) {
             console.error("couldn't forward message to kafka, topic: ", payloads[0].topic ," - error: ", err);
         } else {
-            console.log("forwarded to kafka:")
-            console.log(payloads)
+            console.log("forwarded to kafka:");
+            console.log(payloads);
         }
-    })
-}
-
-function toEvent (message) {
-    try {
-        console.log(message)
-        var event = JSON.parse(message);
-        this.emit(event.type, event.payload);
-    } catch(err) {
-        console.log('not an object' , err);
-    }
+    });
 }
 
 Promise.all([initKafka()]).then(() => {
     initWebSocket().then(() => {
-        console.log("ws gateway available")
-        wsserver.on('connection', function connection(ws) {
-            ws.on('message', toEvent).on('authenticate', function (data) {
-                console.log(data)
-                jwt.verify(data.token, key, function (err, decoded) {
-                    if(!(err != null)) {
-                        console.log(decoded);
-                        //forwardMsg(JSON.stringify(req.body), decoded.sub);
-                        ws.send('OK');
-                    } else {
-                        console.log("403 - Forbidden");
-                        ws.send('Forbidden');
-                    }
-                    console.log(decoded);
-                });
+        console.log("ws gateway available");
+        wsserver.on('connection', (conn, req) => {
+            conn.on('message', (data) => {
+                forwardMsg(data, req.user.sub);
             });
-        })
-    })
+        });
+    });
 })
