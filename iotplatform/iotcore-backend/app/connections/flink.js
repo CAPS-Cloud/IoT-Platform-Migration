a39bd9d799a7c0f0388b1bffb6e4f7dbee5e66c1
Peeranut Chindanonda <pcxd@me.com>
2018-07-15 18:56:06 +0200

    Fix wrong variable reference

--- iotplatform/iotcore-backend/app/connections/flink.js
+++ iotplatform/iotcore-backend/app/connections/flink.js
@@ -12,12 +12,12 @@ const ZOOKEEPER_HOST = require('./zookeeper');
 function addFlinkJob(topic) {
     console.log("Adding flink job", topic);
     return new Promise(function (resolve, reject) {
-        axios.get(`${flink}jars/`).then(response => {
+        axios.get(`${host}jars/`).then(response => {
             if (response.data.files.length > 0) {
                 console.log("Run flink job", topic);
                 const jarId = response.data.files[0].id;
                 const programArgs = `--elasticsearch "${ELASTICSEARCH_HOST_DOMAIN}" --elasticsearch_port ${ELASTICSEARCH_BIN_PORT} --topic ${topic} --bootstrap.servers "${KAFKA_HOST}" --zookeeper.connect "${ZOOKEEPER_HOST}" --groud.id flink_job`;
-                axios.post(`${flink}jars/${jarId}/run?program-args=${encodeURIComponent(programArgs)}`).then(response => {
+                axios.post(`${host}jars/${jarId}/run?program-args=${encodeURIComponent(programArgs)}`).then(response => {
                     console.log("Ran flink job", topic);
                     resolve(response);
                 }).catch(function (err2) {
@@ -37,7 +37,7 @@ function addFlinkJob(topic) {
 
                     request({
                         method: 'post',
-                        url: `${flink}jars/upload`,
+                        url: `${host}jars/upload`,
                         headers: { "Content-Type": "multipart/form-data; boundary=" + boundary },
                         body: Buffer.concat([Buffer.from(data, "utf8"), new Buffer(content, 'binary'), Buffer.from("\r\n--" + boundary + "\r\n", "utf8")]),
                     }, function (err2, response, body) {
@@ -61,14 +61,14 @@ function addFlinkJob(topic) {
 function deleteFlinkJob(topic) {
     console.log("Canceling flink job", topic);
     return new Promise(function (resolve, reject) {
-        axios.get(`${flink}joboverview/`).then(res => {
+        axios.get(`${host}joboverview/`).then(res => {
             const jobs = res.data["running"].concat(res.data["finished"]);
 
             for (var i = 0; i < jobs.length; i++) {
                 const job = jobs[i];
 
                 if (job.name == topic) {
-                    axios.delete(`${flink}jobs/${job.jid}/cancel/`).then(res => {
+                    axios.delete(`${host}jobs/${job.jid}/cancel/`).then(res => {
                         console.log("Done canceling flink job", topic);
                         resolve(res);
                     }).catch(err => reject(err));
